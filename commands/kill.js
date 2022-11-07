b6ff2446b311aae34ec62cfd8c2b641712600183
abbyjng <abigailjng@gmail.com>
2022-11-07 06:23:38 +0000

    fixed up gitignore, adjusted scoreboard to be readable on mobile, and removed config.json from git repo to preserve auth token privacy

--- commands/kill.js
+++ commands/kill.js
@@ -1,40 +1,48 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 const { openDb } = require("./../database_handler.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('kill')
-        .setDescription('Logs a kill.')
-        .addUserOption((option) =>
-            option
-                .setName("killed")
-                .setDescription("The person you killed.")
-                .setRequired(false)
-        ),
-    async execute(interaction) {
-        let db = await openDb();
-        const killed = interaction.options.getUser("killed").username;
-        const textchannel = interaction.guild.channels.cache.get('1037573414413684827');
-        const memberArray = textchannel.members.map(member => member.user.username);
-        if (!memberArray.includes(killed)) {
-            await interaction.reply(`Agent, please stay focused. You just killed a civilian.`);
-            return;
-        }
-        const killer = interaction.user.username;
-        if (killed == killer) {
-            await interaction.reply(`Did you just try to kill yourself agent?`);
-            return;
-        }
-        // gets the scores from all the killer and killed. updates accordingly
-        // killer +2 and killed -1
-        sql = `SELECT score FROM users WHERE username = ?`;
-        killed_score = await db.get(sql, [killed]);
-        killed_score['score'] -= 1;
-        killer_score = await db.get(sql, [killer]);
-        killer_score['score'] += 2;
-        sql = `UPDATE users SET score = ? WHERE username = ?`;
-        await db.run(sql, [killer_score['score'], killer]);
-        await db.run(sql, [killed_score['score'], killed]);
-        await interaction.reply(`You have successfully reported you killed **${killed}**. You now have ${killer_score['score']} points. Watch your back, agent.`);
-    },
+  data: new SlashCommandBuilder()
+    .setName("kill")
+    .setDescription("Logs a kill.")
+    .addUserOption((option) =>
+      option
+        .setName("killed")
+        .setDescription("The person you killed.")
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    let db = await openDb();
+    const killed = interaction.options.getUser("killed").username;
+    const textchannel = interaction.guild.channels.cache.get(
+      "1037090477553500293"
+    );
+    const memberArray = textchannel.members.map(
+      (member) => member.user.username
+    );
+    if (!memberArray.includes(killed)) {
+      await interaction.reply(
+        `Agent, please stay focused. You just killed a civilian.`
+      );
+      return;
+    }
+    const killer = interaction.user.username;
+    if (killed == killer) {
+      await interaction.reply(`Did you just try to kill yourself agent?`);
+      return;
+    }
+    // gets the scores from all the killer and killed. updates accordingly
+    // killer +2 and killed -1
+    sql = `SELECT score FROM users WHERE username = ?`;
+    killed_score = await db.get(sql, [killed]);
+    killed_score["score"] -= 1;
+    killer_score = await db.get(sql, [killer]);
+    killer_score["score"] += 2;
+    sql = `UPDATE users SET score = ? WHERE username = ?`;
+    await db.run(sql, [killer_score["score"], killer]);
+    await db.run(sql, [killed_score["score"], killed]);
+    await interaction.reply(
+      `You have successfully reported you killed **${killed}**. You now have ${killer_score["score"]} points. Watch your back, agent.`
+    );
+  },
 };
--- commands/scoreboard.js
+++ commands/scoreboard.js
@@ -1,33 +1,38 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { openDb } = require("./../database_handler.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('scoreboard')
-        .setDescription('Shows the current scoreboard.'),
-    async execute(interaction) {
-        await interaction.deferReply();
-        let db = await openDb();
-        sql = `SELECT * FROM users`;
-        scores = await db.all(sql);
-        scores.sort(function (a, b) {
-            return b.score - a.score
-        });
-        const embed = new EmbedBuilder()
-            .setTitle('Scores')
-            .setDescription('Who gonna win?');
-        const obj = { agent: '', score: '' };
-        for (let agent of scores) {
-            obj.agent += `\n${agent['username']}`;
-            obj.score += `\n${agent['score']}`;
-        }
-        embed.addFields(
-            { name: `Agent᲼᲼᲼᲼᲼᲼᲼᲼`, value: obj.agent, inline: true },
-            { name: `Score`, value: obj.score, inline: true },
-        );
-        interaction.editReply({
-            embeds: [embed],
-        });
-        return;
-    },
+  data: new SlashCommandBuilder()
+    .setName("scoreboard")
+    .setDescription("Shows the current scoreboard."),
+  async execute(interaction) {
+    await interaction.deferReply();
+    let db = await openDb();
+    sql = `SELECT * FROM users`;
+    scores = await db.all(sql);
+    scores.sort(function (a, b) {
+      return b.score - a.score;
+    });
+    const embed = new EmbedBuilder().setTitle("Scores");
+
+    let description = "Who gonna win?\n";
+    description += "```Agent                Score\n";
+    description += "--------------------------\n";
+
+    for (let agent of scores) {
+      description += `${agent["username"]}`;
+      for (let i = agent["username"].length; i < 21; i++) {
+        description += " ";
+      }
+      description += `${agent["score"]}\n`;
+    }
+    description += "```";
+
+    embed.setDescription(description);
+
+    interaction.editReply({
+      embeds: [embed],
+    });
+    return;
+  },
 };
