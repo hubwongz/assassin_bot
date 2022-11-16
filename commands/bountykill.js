677785554cd4d6678d6d23cb05abdd8c082898a5
hubwongz <hubertw@umich.edu>
2022-11-16 11:06:56 -0500

    whoopsies

--- commands/bountykill.js
+++ commands/bountykill.js
@@ -24,22 +24,24 @@ module.exports = {
 
     // only abby and hubert can do a bounty kill. ids are abby then hubert.
     const agency = interaction.user.id;
-    if (agency != '606339196160049153' || agency != '249389362813468673') {
+    if (agency == '606339196160049153' || agency == '249389362813468673') {
+      //bounty kill:
+      //killer + 4, killed - 3
+      sql = `SELECT nickname, score FROM users WHERE user_id = ?`;
+      killed_score = await db.get(sql, [killed]);
+      killed_score["score"] -= 3;
+      killer_score = await db.get(sql, [killer]);
+      killer_score["score"] += 4;
+      sql = `UPDATE users SET score = ? WHERE user_id = ?`;
+      await db.run(sql, [killer_score["score"], killer]);
+      await db.run(sql, [killed_score["score"], killed]);
+      await interaction.reply(
+        `Bounty on **${killed}** has been collected by **${killer}**. **${killer}**, you now have ${killed_score["score"]} points. You might be the next bounty. Watch your back.`
+      );
+    }
+    else {
       await interaction.reply(`Contact the agency to log your bounty kill.`);
       return;
     }
-    //bounty kill:
-    //killer + 4, killed - 3
-    sql = `SELECT nickname, score FROM users WHERE user_id = ?`;
-    killed_score = await db.get(sql, [killed]);
-    killed_score["score"] -= 3;
-    killer_score = await db.get(sql, [killer]);
-    killer_score["score"] += 4;
-    sql = `UPDATE users SET score = ? WHERE user_id = ?`;
-    await db.run(sql, [killer_score["score"], killer]);
-    await db.run(sql, [killed_score["score"], killed]);
-    await interaction.reply(
-      `Bounty on **${killed}** has been collected by **${killer}**. **${killer}**, you now have ${killed_score["score"]} points. You might be the next bounty. Watch your back.`
-    );
   },
 };
